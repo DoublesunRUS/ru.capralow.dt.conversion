a5828040fcd474e9d5006c3882a91b78efe3b9dc
Александр Капралов <capralow@gmail.com>
2018-08-13 08:49:54 +0300

    Перенос из dev

--- ru.capralow.dt.conversion.plugin.ui/src/ru/capralow/dt/conversion/plugin/ui/ConversionExternalDependenciesModule.java
+++ ru.capralow.dt.conversion.plugin.ui/src/ru/capralow/dt/conversion/plugin/ui/ConversionExternalDependenciesModule.java
@@ -4,7 +4,6 @@ import org.eclipse.core.runtime.Plugin;
 
 import com._1c.g5.v8.dt.bm.index.emf.IBmEmfIndexManager;
 import com._1c.g5.v8.dt.core.platform.IV8ProjectManager;
-import com._1c.g5.v8.dt.md.extension.adopt.IModelObjectAdopter;
 import com._1c.g5.wiring.AbstractServiceAwareModule;
 
 public class ConversionExternalDependenciesModule extends AbstractServiceAwareModule {
@@ -16,7 +15,6 @@ public class ConversionExternalDependenciesModule extends AbstractServiceAwareMo
 	@Override
 	protected void doConfigure() {
 		bind(IV8ProjectManager.class).toService();
-		bind(IModelObjectAdopter.class).toService();
 		bind(IBmEmfIndexManager.class).toService();
 	}
 
