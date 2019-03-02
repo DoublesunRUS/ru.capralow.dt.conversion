6756c66a6cec0ad4359d75b743cb08c62a04fb24
Александр Капралов <capralow@gmail.com>
2019-03-02 21:48:47 +0300

    Заготовка для механизма валидации модуля конвертации

---
+++ ru.capralow.dt.conversion.plugin.ui/src/ru/capralow/dt/conversion/plugin/ui/ConversionModuleXtextBuilderParticipant.java
@@ -0,0 +1,18 @@
+package ru.capralow.dt.conversion.plugin.ui;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ConversionModuleXtextBuilderParticipant implements org.eclipse.xtext.builder.IXtextBuilderParticipant {
+
+	@Override
+	public void build(IBuildContext context, IProgressMonitor monitor) throws CoreException {
+
+		// Object conversionModuleResource = context.getResourceSet().getResource("",
+		// true);
+
+		return;
+
+	}
+
+}
